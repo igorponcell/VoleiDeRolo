8458d2f9dc44d4fadd319b844ffb625791a1f976
Igor Poncellm <igorponcell@gmail.com>
2025-09-08 21:40:00 -0300

    Update About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h2>Sobre nós</h2>
+      <p className={styles.paragraph}>
+        Breve Descrição sobre o volei de rolo
+      </p>
+    </div>
   )
 }
 
